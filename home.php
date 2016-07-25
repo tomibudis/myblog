882bced11a795c48f0263adc41c34ce9b9d7f82f
tomibudis <tomibudis@gmail.com>
2016-07-25 23:01:28 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Toms</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<link rel="shortcut icon" type="image/x-icon" href="assets/images/logos.png"/>
+    <link href='https://bootswatch.com/paper/bootstrap.min.css' rel='stylesheet'/>
+    <!-- Le styles -->
+    <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' rel='stylesheet'/>
+    <link href="assets/css/style.css" rel="stylesheet">
+     <!-- jQuery online menggunakan CDN -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <!-- jQuery lokal -->  
+    <script type="text/javascript" src="/assets/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/bootstrap/js/bootstrap.js"></script>
+    <script id="dsq-count-scr" src="//wwwhellotomsesyes.disqus.com/count.js" async></script>
+
+  </head>
+  <body>
+ 
+	<div class="jumbotron" style="margin-bottom:0px;padding-bottom:0px;"><div class="blur">	<br><center>
+  		<h1 style="margin-bottom:0px;padding-bottom:0px;"><img style="height:160px"src="assets/images/logo.png" class="img-responsive"></h1>
+  		<p style="margin:0px;">This is my blog</p><br>
+		</div>
+	</div>
+
+<nav class="navbar navbar-default">
+<div class="container">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+     
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="home.php">Home <span class="sr-only">(current)</span></a></li>
+        <li><a  href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        
+      </ul>
+      
+      <ul class="nav navbar-nav navbar-right">
+        	
+        <form class="navbar-form navbar-left" role="search">
+        <div class="form-group ">
+          <input type="text"  style="height:30px;margin:3px"class="form-control" placeholder="Search">
+        </div>
+        <button type="submit"class="btn btn-primary btn-sm">Submit</button>
+      </form>
+      </ul>
+    </div>
+  </div>
+  </div>
+</nav>
+
+<div class="container">
+<div class="col-md-4 ">
+	<div class="panel panel-default hover">
+	  <div class="panel-heading">
+	  	<a href="home.php"><h5 style="padding:0px;margin-bottom:10px;margin-top:3px">Wisata Jemabatan gantole</h5></a>
+		<p style="margin:0px;padding:0px"><i class="fa fa-user"></i>&nbsp;&nbsp;by:Toms &nbsp;&nbsp;&nbsp;
+	  	<i class="fa fa-calendar-o">&nbsp;&nbsp;December 2, 2013</i>
+	  	<a href="#" style="color:grey;float:right;padding:0px"><i class="fa fa-comments fa-2x"> </i> 23</a> </p>
+		</div>
+		
+	  <div class="panel-body" style="margin:0px;padding:0px">
+	  	 <a href="home.php"><img style="margin:0px;padding:0px;border-radius:0px; " href="home.php"src="https://static.wixstatic.com/media/41d000_b98614b6675947b4a7142615d5e72470.jpg/v1/fill/w_660,h_440,al_c,q_80,usm_0.66_1.00_0.01/41d000_b98614b6675947b4a7142615d5e72470.jpg" class="img-thumbnail  img-rersponsive">  			
+	  	</a>
+	  </div>
+	  <div class="panel panel-default">
+		  <div class="panel-body"><p>Anda Juga dapat menemukan akun social media saya dengan mengklik tautan dibawah </p>
+		  <button type="button" class="btn btn-primary btn-sm">Continu reading</button>
+      
+		  </div>
+		</div>
+	</div>
+</div>
+<div class="col-md-4 ">
+	<div class="panel panel-default hover">
+	  <div class="panel-heading">
+	  	<a href="home.php"><h5 style="padding:0px;margin-bottom:10px;margin-top:3px">Wisata Jemabatan gantole</h5></a>
+		<p style="margin:0px;padding:0px"><i class="fa fa-user"></i>&nbsp;&nbsp;by:Toms &nbsp;&nbsp;&nbsp;
+	  	<i class="fa fa-calendar-o">&nbsp;&nbsp;December 2, 2013</i>
+	  	<a href="#" style="color:grey;float:right;padding:0px"><i class="fa fa-comments fa-2x"> </i> 23</a> </p>
+		</div>
+		
+	  <div class="panel-body" style="margin:0px;padding:0px">
+	  	 <a href="home.php"><img style="margin:0px;padding:0px;border-radius:0px; " href="home.php"src="assets/images/header.jpg" class="img-thumbnail  img-rersponsive">  			
+	  	</a>
+	  </div>
+	  <div class="panel panel-default">
+		  <div class="panel-body"><p>Anda Juga dapat menemukan akun social media saya dengan mengklik tautan dibawah </p>
+		  <button type="button" class="btn btn-primary btn-sm">Continu reading</button>
+		  
+		  </div>
+		</div>
+	</div>
+</div>
+
+</div>
+</div>
+<center>
+<ul class="pagination">
+  <li class="disabled"><a href="#">&laquo;</a></li>
+  <li class="active"><a href="#">1</a></li>
+  <li><a href="#">2</a></li>
+  <li><a href="#">3</a></li>
+  <li><a href="#">4</a></li>
+  <li><a href="#">5</a></li>
+  <li><a href="#">&raquo;</a></li>
+</ul>
+</div>
+</center>
+<div class="progress"style="margin-bottom:0px;padding:0px">
+  <div class="progress-bar" style="width: 100%;"></div>
+</div>
+<div class="jumbotron1" style="margin-bottom:0px;padding-bottom:0px;"><div class="footer">	<br><center>
+	<h6>&copy; Copyright 2016</h6>
+  <p>Created by:Tomi budi susilo /13523233</p>
+  <br>
+  
+</div></div>
+</html>
